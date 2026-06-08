c5a108b6d4b794c7b86d67a844030b30e3c1785a
Srikant Panda <srikantap571@gmail.com>
2026-06-08 07:45:44 +0530

    vercel fix

--- artifacts/js-learn/vite.config.ts
+++ artifacts/js-learn/vite.config.ts
@@ -50,6 +50,8 @@ export default defineConfig({
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
+    sourcemap: false,
+    chunkSizeWarningLimit: 2000,
   },
   server: {
     port,
